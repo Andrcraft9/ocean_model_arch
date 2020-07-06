e34eb63cd1c06e77d2fb092b44924952aba6cfa2
Andrcraft9 <achaplygin99@gmail.com>
2020-07-07 02:06:46 +0300

    Sync in developing, part 2. Need to pack buffer, send it, recv it and unpack. Copy inner blocks + boundary blocks in the same rank is completed.

--- shared/mpp/sync.f90
+++ shared/mpp/sync.f90
@@ -32,8 +32,9 @@ module mpp_sync_module
     public :: sync
 
     ! Buffers for each near rank
-    real(wp8), allocatable :: sync_buf8(:, :)
-    real(wp4), allocatable :: sync_buf4(:, :)
+    real(wp8), allocatable :: sync_buf_r8(:, :)
+    real(wp4), allocatable :: sync_buf_r4(:, :)
+    integer, allocatable :: sync_buf_pos(:)  ! Position of buffers, need for prepare buffers. Position of buffer for each near rank
 
 !------------------------------------------------------------------------------
 
@@ -54,87 +55,94 @@ contains
     subroutine allocate_mpp_sync_buffers(domain)
         type(domain_type), intent(in) :: domain
 
+        integer :: k, kk
+        integer :: sync_dir(8)
+        integer :: k_dir(8)
+        integer :: rank_dir(8)
+        integer :: nxs, nxe, nys, nye     ! Boundary points
+        integer, allocatable :: buf_sizes(:)
+        integer :: max_buf_size = 1
+
+        allocate(buf_sizes(mpp_count))
+        buf_sizes = 0
+
+        do k = domain%start_boundary, domain%start_boundary + domain%bcount_boundary - 1
+
+            sync_dir(1) = _NXP_; k_dir(1) = domain%blocks_info(k)%k_nxp; rank_dir(1) = domain%blocks_info(k)%rank_nxp
+            sync_dir(2) = _NXM_; k_dir(2) = domain%blocks_info(k)%k_nxm; rank_dir(2) = domain%blocks_info(k)%rank_nxm
+            sync_dir(3) = _NYP_; k_dir(3) = domain%blocks_info(k)%k_nyp; rank_dir(3) = domain%blocks_info(k)%rank_nyp
+            sync_dir(4) = _NYM_; k_dir(4) = domain%blocks_info(k)%k_nym; rank_dir(4) = domain%blocks_info(k)%rank_nym
+
+            sync_dir(5) = _NXP_NYP_; k_dir(5) = domain%blocks_info(k)%k_nxp_nyp; rank_dir(5) = domain%blocks_info(k)%rank_nxp_nyp
+            sync_dir(6) = _NXP_NYM_; k_dir(6) = domain%blocks_info(k)%k_nxp_nym; rank_dir(6) = domain%blocks_info(k)%rank_nxp_nym
+            sync_dir(7) = _NXM_NYP_; k_dir(7) = domain%blocks_info(k)%k_nxm_nyp; rank_dir(7) = domain%blocks_info(k)%rank_nxm_nyp
+            sync_dir(8) = _NXM_NYM_; k_dir(8) = domain%blocks_info(k)%k_nxm_nym; rank_dir(8) = domain%blocks_info(k)%rank_nxm_nym
+
+            do kk = 1, 8
+                if (rank_dir(kk) >= 0) then
+                    if (rank_dir(kk) /= mpp_rank) then
+                        call get_boundary_points_of_block(domain, k, sync_dir(kk), nxs, nxe, nys, nye)
+                        buf_sizes(rank_dir(kk)) = buf_sizes(rank_dir(kk)) + 1 + (nxe - nxs + 1) + (nye - nys + 1)
+                    endif
+                endif
+            enddo
+        enddo
+
+        do k = 1, mpp_count
+            if (max_buf_size < buf_sizes(k)) max_buf_size = buf_sizes(k)
+        enddo
+        deallocate(buf_sizes)
+
+        if (debug_level >= 5) then
+            print *, "MPP SYNC INFO: rank, max buffer size = ", mpp_rank, max_buf_size
+        endif
+
+        allocate(sync_buf_r4(max_buf_size, mpp_count))
+        allocate(sync_buf_r8(max_buf_size, mpp_count))
+        allocate(sync_buf_pos(mpp_count))
+        sync_buf_r4 = 0; sync_buf_r8 = 0
+        sync_buf_pos = 1
+
+        call mpp_sync_output()
     end subroutine
 
     subroutine deallocate_mpp_sync_buffers(domain)
         type(domain_type), intent(in) :: domain
-        
+
+        deallocate(sync_buf_r4)
+        deallocate(sync_buf_r8)
+        deallocate(sync_buf_pos)
     end subroutine
+
+!------------------------------------------------------------------------------
+! Generic subroutines:
 
         subroutine syncborder_data2D_real8(domain, data2d)
 #define _MPI_TYPE_ mpi_real8
+#define _SYNC_BUF_ sync_buf_r8
         
         implicit none
         type(data2D_real8_type), intent(inout) :: data2d
         type(domain_type), intent(in) :: domain
 
-        integer :: k, kk
-        integer :: sync_dir(8)
-        integer :: k_dir(8)
-        integer :: nxs, nxe, nys, nye     ! Boundary points
-        integer :: hnxs, hnxe, hnys, hnye ! Halo points
-
-        ! Update halo for inner blocks (only copy)
-        !
-
-        do k = domain%start_inner, domain%start_inner + domain%bcount_inner - 1
-            
-            sync_dir(1) = _NXP_; k_dir(1) = domain%blocks_info(k)%k_nxp
-            sync_dir(2) = _NXM_; k_dir(2) = domain%blocks_info(k)%k_nxm
-            sync_dir(3) = _NYP_; k_dir(3) = domain%blocks_info(k)%k_nyp
-            sync_dir(4) = _NYM_; k_dir(4) = domain%blocks_info(k)%k_nym
-
-            sync_dir(5) = _NXP_NYP_; k_dir(5) = domain%blocks_info(k)%k_nxp_nyp
-            sync_dir(6) = _NXP_NYM_; k_dir(6) = domain%blocks_info(k)%k_nxp_nym
-            sync_dir(7) = _NXM_NYP_; k_dir(7) = domain%blocks_info(k)%k_nxm_nyp
-            sync_dir(8) = _NXM_NYM_; k_dir(8) = domain%blocks_info(k)%k_nxm_nym
-            
-            do kk = 1, 8
-                call get_boundary_points_of_block(domain, k_dir(kk), get_inverse_dir(sync_dir(kk)), nxs, nxe, nys, nye)
-                call get_halo_points_of_block(domain, k, sync_dir(kk), hnxs, hnxe, hnys, hnye)
-                data2d%block(k)%field(hnxs : hnxe, hnys : hnye) = data2d%block(k_dir(kk))%field(nxs : nxe, nys : nye)
-            enddo
-        enddo
+#include    "syncborder_block2D_gen.fi"
 
 #undef _MPI_TYPE_
-    end subroutine
+#undef _SYNC_BUF_
+        end subroutine
 
-    subroutine syncborder_data2D_real4(domain, data2d)
+        subroutine syncborder_data2D_real4(domain, data2d)
 #define _MPI_TYPE_ mpi_real4
-        
+#define _SYNC_BUF_ sync_buf_r4
+
         implicit none
         type(data2D_real4_type), intent(inout) :: data2d
         type(domain_type), intent(in) :: domain
 
-        integer :: k, kk
-        integer :: sync_dir(8)
-        integer :: k_dir(8)
-        integer :: nxs, nxe, nys, nye     ! Boundary points
-        integer :: hnxs, hnxe, hnys, hnye ! Halo points
-
-        ! Update halo for inner blocks (only copy)
-        !
-
-        do k = domain%start_inner, domain%start_inner + domain%bcount_inner - 1
-            
-            sync_dir(1) = _NXP_; k_dir(1) = domain%blocks_info(k)%k_nxp
-            sync_dir(2) = _NXM_; k_dir(2) = domain%blocks_info(k)%k_nxm
-            sync_dir(3) = _NYP_; k_dir(3) = domain%blocks_info(k)%k_nyp
-            sync_dir(4) = _NYM_; k_dir(4) = domain%blocks_info(k)%k_nym
-
-            sync_dir(5) = _NXP_NYP_; k_dir(5) = domain%blocks_info(k)%k_nxp_nyp
-            sync_dir(6) = _NXP_NYM_; k_dir(6) = domain%blocks_info(k)%k_nxp_nym
-            sync_dir(7) = _NXM_NYP_; k_dir(7) = domain%blocks_info(k)%k_nxm_nyp
-            sync_dir(8) = _NXM_NYM_; k_dir(8) = domain%blocks_info(k)%k_nxm_nym
-            
-            do kk = 1, 8
-                call get_boundary_points_of_block(domain, k_dir(kk), get_inverse_dir(sync_dir(kk)), nxs, nxe, nys, nye)
-                call get_halo_points_of_block(domain, k, sync_dir(kk), hnxs, hnxe, hnys, hnye)
-                data2d%block(k)%field(hnxs : hnxe, hnys : hnye) = data2d%block(k_dir(kk))%field(nxs : nxe, nys : nye)
-            enddo
-        enddo
+#include    "syncborder_block2D_gen.fi"
 
 #undef _MPI_TYPE_
-            end subroutine
+#undef _SYNC_BUF_
+        end subroutine
 
 end module mpp_sync_module
